fa3b2bd4498193410c93b58be0ada8b0e68aa5e5
ny19tóthz <ny19tthz@DESKTOP-VE7ML0E>
2023-11-10 11:19:25 +0100

    Add project files.

---
+++ felevieredmenyek/Class.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace felevieredmenyek
+{
+    class Class
+    {
+        public string TanuloNev { get; set; }
+        public double OktatasiAzonosito { get; set; }
+        List<int> jegyek { get; set; }
+
+
+
+        public Class(string adatok)
+        {
+            string[] adat = adatok.Split("\t");
+            TanuloNev = adat[0];
+            OktatasiAzonosito = Convert.ToDouble(adat[1]);
+            jegyek = new List<int>();
+
+            for (int i = 2; i < adat.Length; i++)
+            {
+                jegyek.Add(Convert.ToInt32(adat[i]));
+            }
+
+        }
+        public override string ToString()
+        {
+            return $"Tanuló neve: {TanuloNev},Oktatási Azonosító: {OktatasiAzonosito}";
+        }
+        public double atlag(double atlag)
+        {
+            foreach (var tanulo in jegyek)
+            {
+                atlag += tanulo;
+
+            }
+            return atlag / jegyek.Count;
+        }
+
+    }
+}
+
---
+++ felevieredmenyek/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace felevieredmenyek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Class> tanulok = new List<Class>();
+            //1. feladat
+            //Írjunk függvényt, ami kiszámítja a jegyek átlagát tanulónként.Írjunk függvényt, ami kiszámítja az osztályátlagot, illetve a tantárgyankénti átlagot is.
+            var sr = new StreamReader(
+                path: @"..\..\..\adatok.txt",
+                encoding: System.Text.Encoding.UTF8
+                );
+            _ = sr.ReadLine();
+          
+            while (!sr.EndOfStream)
+            {
+                _ = sr.ReadLine();
+                var sor = new Class(sr.ReadLine());
+                tanulok.Add(sor);
+
+            }
+            double no = 0;
+            foreach ( var tanulo in tanulok)
+            {
+                Console.WriteLine($"{tanulo.ToString()},Átlag: {tanulo.atlag(no)}");
+            }
+            //2. feladat: Programozás gyakorlatból megbukottak adatainak kiiratása.
+            //3. feladat: Írjunk függvényt, amivel keressük meg az első olyan embert, akinek hármasa van angol nyelvből, majd írjuk ki az adatait.
+            //4. feladat: Kérjük be a felhasználótól, melyik tanuló legjobb jegyét szeretné megtudni.
+            //Írjuk ki az adott tanuló legjobb eredményét függvénnyel. Szorgalmi: Kezeljük a lehetséges felhasználói hibát is.
+            //5. feladat: Egy új fájlba írjuk ki a fenti tanuló nevét és oktatási azonosítóját.
+        }
+    }
+}
